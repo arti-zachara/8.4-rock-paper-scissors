a4710c6372b32d0942c1d757a1fa42ef6fe8a807
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-03-12 18:55:14 +0100

    Add computer move script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var playerMoveChoice;
+var computerMoveChoice;
 
 // providing game's output
 var output = document.getElementById('output');
@@ -9,6 +10,21 @@ var displayText = function(text){
 }
 displayText('Game result is ' +playerMoveChoice);
 
+// Computer move
+var computerMove = function(){
+    var computerMoveRandom = Math.floor(Math.random() * 3) + 1;
+    switch(computerMoveRandom) {
+        case 1:
+            computerMoveChoice = 'rock';
+            return computerMoveChoice; 
+        case 2:
+            computerMoveChoice = 'paper';
+            return computerMoveChoice;
+        case 3:
+            computerMoveChoice = 'scissors';
+            return computerMoveChoice;
+    }
+}
 
 // Buttons allowing player to chose a move
 var playerMoveRock = document.getElementById('playerMove_rock'),
@@ -17,15 +33,22 @@ playerMoveScissors = document.getElementById('playerMove_scissors');
 
 playerMoveRock.addEventListener('click', function() {
     playerMoveChoice = 'rock';
+    computerMove();
+    displayText('Computer has played ' + computerMoveChoice);
     displayText('You have played ' + playerMoveChoice);
 });
 playerMovePaper.addEventListener('click', function() {
     playerMoveChoice = 'paper'; 
+    computerMove();
+    displayText('Computer has played ' + computerMoveChoice);
     displayText('You have played ' + playerMoveChoice);
-    
 });
 playerMoveScissors.addEventListener('click', function() {
     playerMoveChoice = 'scissors';
+    computerMove();
+    displayText('Computer has played ' + computerMoveChoice);
     displayText('You have played ' + playerMoveChoice);
 });
+
+
 
