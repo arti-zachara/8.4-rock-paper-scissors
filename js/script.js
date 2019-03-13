dc632ee1a927666b506af1a648e7408233fb681f
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-03-13 22:55:14 +0100

    Add determining the result to script

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 "use strict";
 
-var playerMoveChoice;
+var gameResult;
 
 // providing game's output
 var output = document.getElementById("output");
 var displayText = function(text) {
-  output.insertAdjacentHTML("afterbegin", "result: " + text + "<br>");
+  output.insertAdjacentHTML("afterbegin", text + "<br>");
 };
-displayText("Game result is " + playerMoveChoice);
 
-// Computer move
-var computerMove = function() {
+// Buttons allowing player to chose a move
+var playerMoveRock = document.getElementById("playerMove_rock"),
+  playerMovePaper = document.getElementById("playerMove_paper"),
+  playerMoveScissors = document.getElementById("playerMove_scissors");
+
+// invoking playerMove function with a payer's choice parameter
+playerMoveRock.addEventListener("click", function() {
+  playerMove("rock");
+});
+playerMovePaper.addEventListener("click", function() {
+  playerMove("paper");
+});
+playerMoveScissors.addEventListener("click", function() {
+  playerMove("scissors");
+});
+
+// Computer move function randomizing the move
+function computerMove() {
   var computerMoveRandom = Math.floor(Math.random() * 3) + 1;
   switch (computerMoveRandom) {
     case 1:
@@ -20,20 +35,30 @@ var computerMove = function() {
     case 3:
       return "scissors";
   }
-};
+}
 
-var playerMove = function(playerMove) {
-  var playerMoveChoice = playerMove;
+// game mechanics invoked after player's choice
+function playerMove(playerMoveChosen) {
+  var playerMoveChoice = playerMoveChosen;
   var computerMoveChoice = computerMove();
-  displayText("Computer has played " + computerMoveChoice);
-  displayText("You have played " + playerMoveChoice);
-};
-
-// Buttons allowing player to chose a move
-var playerMoveRock = document.getElementById("playerMove_rock"),
-  playerMovePaper = document.getElementById("playerMove_paper"),
-  playerMoveScissors = document.getElementById("playerMove_scissors");
-
-playerMoveRock.addEventListener("click", playerMove("rock"));
-playerMovePaper.addEventListener("click", playerMove("paper"));
-playerMoveScissors.addEventListener("click", playerMove("scissors"));
+  if (playerMoveChoice === computerMoveChoice) {
+    gameResult = "Noone";
+  } else if (
+    (playerMoveChoice === "paper" && computerMoveChoice === "rock") ||
+    (playerMoveChoice === "rock" && computerMoveChoice === "scissor") ||
+    (playerMoveChoice === "scissor" && computerMoveChoice === "paper")
+  ) {
+    gameResult = "You";
+  } else {
+    gameResult = "Computer";
+  }
+  displayText(
+    "<strong>" +
+      gameResult +
+      "</strong> won. You played <strong>" +
+      playerMoveChoice +
+      "</strong>, computer played <strong>" +
+      computerMoveChoice +
+      "</strong>"
+  );
+}
